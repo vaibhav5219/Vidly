52bdc54bd17416000aa3412de0b038ec131ba1ab
Vaibhav <vaibhavkumar5219@gmail.com>
2022-07-23 12:55:07 +0530

    New RentalForm DropDown List added and Confirmation Page addes

--- vidli_Full_App/Vidly/Controllers/RentalsController.cs
+++ vidli_Full_App/Vidly/Controllers/RentalsController.cs
@@ -22,11 +22,33 @@ namespace Vidly.Controllers
         public ViewResult New()
         {
             //public List<int> movieIds = _context.Movie();
-            var viewModel = new RentalFormViewModel
+            var viewModel = new RentalFormViewModel();
+
+            var moviesList =_context.Movies.ToList();
+            var mList = new List<SelectListItem>();
+            foreach(var movie in moviesList)
             {
-                CustomerId = 1,
-                MovieId = 1
-            };
+                var selectList = new SelectListItem
+                {
+                    Text = movie.Name,
+                    Value = movie.Id.ToString()
+                };
+                mList.Add(selectList);
+            }
+
+            var customerLists = _context.Customers.ToList();
+            var cList = new List<SelectListItem>();
+            foreach (var customer in customerLists)
+            {
+                var selectList = new SelectListItem
+                {
+                    Text = customer.Name,
+                    Value = customer.Id.ToString()
+                };
+                cList.Add(selectList);
+            }
+            viewModel.Movies = mList;
+            viewModel.Customers = cList;
 
             return View("RentalForm", viewModel);
         }
@@ -36,22 +58,29 @@ namespace Vidly.Controllers
         [HttpPost]
         public ViewResult New(RentalFormViewModel newRental)
         {
-            var customer = _context.Customers.Single(c => c.Id == newRental.CustomerId);
-            var movie = _context.Movies.Single(c => c.Id == newRental.MovieId); 
-
-            var viewModel = new RentalFormViewModel
+            if (!ModelState.IsValid)
             {
-                CustomerId = 1,
-                MovieId = 1
-            };
-             if (movie.NumberAvailable == 0)        // Edge Case => Optimistic
+                return View("RentalForm");
+            }
+            int SCID = int.Parse(newRental.SelectedCustomerID);
+            var customer = _context.Customers.Single(c => c.Id == SCID);
+
+            int SMID = int.Parse(newRental.SelectedMovieID);
+            var movie = _context.Movies.Single(c => c.Id == SMID);
+
+
+            ViewBag.NumberAvail = movie.NumberAvailable;
+            ViewBag.Success = false;
+            if (movie.NumberAvailable == 0)        // Edge Case => Optimistic
              {
-                return View("RentalForm", viewModel);
+                return View("ConfirmPage");
              }
 
              movie.NumberAvailable--;  // decreasing 
+             ViewBag.Success = true;
+             ViewBag.NumberAvail--;
 
-             var rental = new Rental  // rental object
+            var rental = new Rental  // rental object
              {
                     Customer = customer,
                     Movie = movie,
@@ -60,7 +89,7 @@ namespace Vidly.Controllers
              _context.Rentals.Add(rental);
              _context.SaveChanges();
             
-            return View("RentalForm", viewModel);
+            return View("ConfirmPage");
         }
     }
 }
